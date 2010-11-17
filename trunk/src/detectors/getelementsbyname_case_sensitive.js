ff7d2f2f78150bc23dd4a3a1d0e18a48c09bbbda
wangjun02@beyondsoft.com <wangjun02@beyondsoft.com@5629863a-3974-ef24-9cdc-758d184f6d3f>
2010-11-17 06:19:42 +0000

    init add

---
+++ trunk/src/detectors/getelementsbyname_case_sensitive.js
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2010 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+addScriptToInject(function() {
+
+chrome_comp.CompDetect.declareDetector(
+
+'getelementsbyname_case_sensitive',
+
+chrome_comp.CompDetect.NonScanDomBaseDetector,
+
+/*
+ * step of reporting problem:
+ * 1. Listen document.getElementsByName method, when it's called and get no result:
+ *   1.1 The element with name attribute:
+ *     1.1.1 attribute value toLowerCase() equals to argument of getElementsByName toLowerCase()
+ */
+
+function constructor(rootNode) {
+  var that = this;
+
+  this.getElementsByNameHandle_ = function(result, originalArguments, callStack) {
+    if(result.length == 0){
+      var elements = document.querySelectorAll("[name]"),
+          name = originalArguments[0];
+      Array.prototype.forEach.call(elements, function(n){
+        if(n.getAttribute("name").toLowerCase() == name.toLowerCase()){
+          that.addProblem('SD9012', {
+            nodes: [this],
+            details: "document.getElementsByName(" + name + ")",
+            needsStack: true
+          });
+          return;
+        }
+      });
+    }
+  };
+},
+
+function setUp() {
+  chrome_comp.CompDetect.registerExistingMethodHook(
+      Document.prototype, 'getElementsByName', this.getElementsByNameHandle_);
+},
+
+function cleanUp() {
+  chrome_comp.CompDetect.unregisterExistingMethodHook(
+      Document.prototype, 'getElementsByName', this.getElementsByNameHandle_);
+}
+); // declareDetector
+
+});
