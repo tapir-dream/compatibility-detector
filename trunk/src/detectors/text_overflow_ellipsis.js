55a2424ac59246bf5214285144d5de0dbbe879cc
duanlixin@beyondsoft.com <duanlixin@beyondsoft.com@5629863a-3974-ef24-9cdc-758d184f6d3f>
2010-12-07 09:47:54 +0000

    判断子元素是否大于父元素宽度，排除子元素是非block情况，排除子元素是<a>等标签并设置float的情况，判断父元素是否设置宽度。

--- trunk/src/detectors/text_overflow_ellipsis.js
+++ trunk/src/detectors/text_overflow_ellipsis.js
@@ -25,38 +25,64 @@ chrome_comp.CompDetect.ScanDomBaseDetector,
 null, // constructor
 
 function checkNode(node, context) {
-  if (Node.ELEMENT_NODE != node.nodeType)
+  if (Node.ELEMENT_NODE != node.nodeType ||context.isDisplayNone())
     return;
-
-  var style = chrome_comp.getComputedStyle(node);
-  var textOverflow = chrome_comp.getComputedStyle(node).textOverflow;
-  var overflow = chrome_comp.getComputedStyle(node).overflow;
-  var wordWrap = chrome_comp.getComputedStyle(node).wordWrap;
-  //Increase the filter conditions , When the elements
-  //of the TD and set word-wrap:break-word ,no problem.
-  if (style && textOverflow == 'ellipsis' && overflow == "hidden" && 
-    node.tagName !="TD" && wordWrap != "break-word") {
-    var nodeWidth = chrome_comp.getComputedStyle(node).width;
-    var childrenWidth= null;
-    for(var i=0,c=node.childNodes.length;i<c;i++){
-      //If the child element is a block element, 
-      //there may be compatibility issues
-      if(node.childNodes[i].nodeType!==3 && chrome_comp.getComputedStyle
-        (node.childNodes[i]).display=="block"){
+  //Recursively check if the length of sub-elements than the parent element
+  function loopForNode(nodeElement,nodeWidth){
+   var node = nodeElement;
+   if(node.childNodes.length>0){
+    for(var i=0;i<node.childNodes.length;i++){
+     if(node.childNodes[i].nodeType!=3){
+      if(node.childNodes[i].childNodes.length==1 && 
+        node.childNodes[i].childNodes[0].nodeType==3){
+        //debugger;
+        if(window.getComputedStyle(node.childNodes[i]).display == "block" && 
+          window.getComputedStyle(node.childNodes[i]).float == "none"){
+        console.log(node.childNodes[i].id);
         var tempNode= node.childNodes[i].cloneNode(true);
         //To obtain the actual length of the block elements
         tempNode.style.float= "right";
         document.body.appendChild(tempNode);
-        childrenWidth = chrome_comp.getComputedStyle(tempNode).width;
+        childrenWidth = window.getComputedStyle(tempNode).width;
         //Remove the temporary elements
         document.body.removeChild(tempNode);
-        //If the child element is greater than the actual length of the parent 
-        //element length, there are compatibility issues
-        if(parseInt(childrenWidth,10)>parseInt(nodeWidth,10)){
-          this.addProblem('RT3005', [node]);
-        }
+        console.log(childrenWidth);
+        console.log(nodeWidth);
+        if(parseInt(childrenWidth)>parseInt(nodeWidth)){
+          //debugger;
+          This.flag=true;
+          return;
+        }}
       }
+      if(node.childNodes.length>0)
+       loopForNode(node.childNodes[i],nodeWidth,this.flag);
+     }
     }
+   }
+  }
+  //Check whether the element to set the width
+  function isAutoWidth(element) {
+    var inlineDisplay = element.style.display;
+    element.style.display = 'none !important';
+    var width = chrome_comp.getComputedStyle(element).width;
+    element.style.display = null;
+    element.style.display = (inlineDisplay) ? inlineDisplay : null;
+    return width == 'auto';
+  }
+  var This = this;
+  var style = chrome_comp.getComputedStyle(node);
+  var textOverflow = style.textOverflow;
+  var overflow = style.overflow;
+  var wordWrap = style.wordWrap;
+  //Increase the filter conditions , When the elements
+  //of the TD and set word-wrap:break-word ,no problem.
+  if (style && textOverflow == 'ellipsis' && overflow == "hidden" && 
+    node.tagName !="TD" && wordWrap != "break-word" && !isAutoWidth(node)) {
+    var nodeWidth = chrome_comp.getComputedStyle(node).width;
+    This.flag = false;
+    loopForNode(node,nodeWidth);
+    if(This.flag == true)
+      this.addProblem('RT3005', [node]);
   }
 }
 
