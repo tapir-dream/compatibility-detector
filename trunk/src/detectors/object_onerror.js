07067ec917452f444562dc54f03fb6a7dab0de05
wangjun02@beyondsoft.com <wangjun02@beyondsoft.com@5629863a-3974-ef24-9cdc-758d184f6d3f>
2010-12-13 07:02:41 +0000

    improve code style.

--- trunk/src/detectors/object_onerror.js
+++ trunk/src/detectors/object_onerror.js
@@ -24,19 +24,6 @@ chrome_comp.CompDetect.ScanDomBaseDetector,
 
 null, // constructor
 
-/*
- *【思路】
- * 检测所有 OBJECT 的元素
- * 判断其是否有 onerror 属性或者其 DOM 对象上是否通过 onerror 绑定了事件
- * 同时判断其 classid 属性是否为那几种 Chrome 支持的插件 (classid 由 WebKit 源代码中得到)，若为其他 classid 则发出警告
- *
- *【缺陷】
- * 判断 classid 可能不准确
- * IE 中可能存在其他绑定 onerror 事件的方式 (经测试  attachEvent 无法绑定)
- *
- */
-
-
 function checkNode(node, additionalData) {
   if (Node.ELEMENT_NODE != node.nodeType)
     return;
@@ -45,12 +32,12 @@ function checkNode(node, additionalData) {
     return;
 
   var supportedClassid = [
-      "CLSID:D27CDB6E-AE6D-11CF-96B8-444553540000",
-      "CLSID:CFCDAA03-8BE4-11CF-B84B-0020AFBBCCFA",
-      "CLSID:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B",
-      "CLSID:166B1BCA-3F9C-11CF-8075-444553540000",
-      "CLSID:6BF52A52-394A-11D3-B153-00C04F79FAA6",
-      "CLSID:22D6F312-B0F6-11D0-94AB-0080C74C7E95",
+    'CLSID:D27CDB6E-AE6D-11CF-96B8-444553540000',
+    'CLSID:CFCDAA03-8BE4-11CF-B84B-0020AFBBCCFA',
+    'CLSID:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B',
+    'CLSID:166B1BCA-3F9C-11CF-8075-444553540000',
+    'CLSID:6BF52A52-394A-11D3-B153-00C04F79FAA6',
+    'CLSID:22D6F312-B0F6-11D0-94AB-0080C74C7E95'
   ];
   if (!node.getAttribute('onerror') || !node.onerror)
     return;
