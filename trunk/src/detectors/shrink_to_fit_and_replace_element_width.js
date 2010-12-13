bb6c7675a42814709bed66ada66f95dbe637cd28
duanlixin@beyondsoft.com <duanlixin@beyondsoft.com@5629863a-3974-ef24-9cdc-758d184f6d3f>
2010-12-13 06:58:12 +0000

    版权，detector名字，80

--- trunk/src/detectors/shrink_to_fit_and_replace_element_width.js
+++ trunk/src/detectors/shrink_to_fit_and_replace_element_width.js
@@ -1,31 +1,29 @@
-// @author : qianbaokun@gmail.com
+/*
+ * Copyright 2010 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 addScriptToInject(function() {
 
 chrome_comp.CompDetect.declareDetector(
 
-'shrinkToFitAndReplaceElementWidth',
+'shrink_to_fit_and_replace_element_width',
 
 chrome_comp.CompDetect.ScanDomBaseDetector,
 
 null, // constructor
 
-/*
- *【思路】
- * 检测所有标记，过滤出标记为替换元素(+MARQUEE)，
- * 如果 INPUT SELECT BUTTON IFRAME IMG OBJECT 的设置宽度为百分比，或者是未设定宽度的 MARQUEE 元素，则查找他们的父元素。
- * 如果他们父元素为 isShrikToFit ，则命中。
- *
- *【缺陷】
- *首先此检测依赖 chrome_comp.isShrinkToFit 方法的准确性，
- *以及 chrome_comp.getDefinedStylePropertyByName 方法准确性，
- *现已知 getDefinedStylePropertyByName 方法存在样式跨域获取问题，导致 isShrinkToFit 方法判断可能不准确。
- *
- *【messages.json】
- * "RD1021": { "message": "各浏览器中当容器元素 shrink-to-fit 时容器内 MARQUEE 标签的计算宽度不一致"},
- * "RD1021_suggestion": { "message": "给 MARQUEE 元素定义具体的宽度，保证各浏览器兼容。"},
- *
- */
 
 function checkNode(node, context) {
 
@@ -40,7 +38,8 @@ function checkNode(node, context) {
 
   //detector marquee element
   if ( node.tagName == 'MARQUEE' ){
-     elementWidth = chrome_comp.getDefinedStylePropertyByName(node, false, 'width');
+     elementWidth = chrome_comp.getDefinedStylePropertyByName(node,
+       false, 'width');
      if ( elementWidth != undefined ) return;
      if ( !chrome_comp.isShrinkToFit(getParentElement(node)) ) return;
 
@@ -50,7 +49,8 @@ function checkNode(node, context) {
   }
 
   //detector replace element element
-  elementWidth = chrome_comp.getDefinedStylePropertyByName(node, false, 'width');
+  elementWidth = chrome_comp.getDefinedStylePropertyByName(node,
+    false, 'width');
   if ( elementWidth == undefined ) return ;
   if ( elementWidth.toString().slice(-1) != "%" ) return;
   if ( !chrome_comp.isShrinkToFit(getParentElement(node)) ) return;
