2a4d07c2b6cffd25470fb5c8c23ce81103a6906a
wangjun02@beyondsoft.com <wangjun02@beyondsoft.com@5629863a-3974-ef24-9cdc-758d184f6d3f>
2010-12-13 03:23:21 +0000

    improve code style.

--- trunk/src/detectors/invisible_element_overflow.js
+++ trunk/src/detectors/invisible_element_overflow.js
@@ -1,32 +1,39 @@
-// @author : luyuan.china@gmail.com
+/*
+ * Copyright 2010 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 addScriptToInject(function() {
 
 chrome_comp.CompDetect.declareDetector(
 
-'invisibleElementOverflow',
+'invisible_element_overflow',
 
 chrome_comp.CompDetect.ScanDomBaseDetector,
 
 null, // constructor
 
-/*【思路】
- * 检测所有宽度或者高度为 0 的元素
- * 判断其包含块的 overflow 特性是否为 auto 或 scroll
- *
- */
-
-
 function checkNode(node, additionalData) {
   function isInvisible(nodeEl) {
-    var w = parseInt(window.chrome_comp.getComputedStyle(nodeEl).width),
-        h = parseInt(window.chrome_comp.getComputedStyle(nodeEl).height);
+    var w = parseInt(window.chrome_comp.getComputedStyle(nodeEl).width);
+    var h = parseInt(window.chrome_comp.getComputedStyle(nodeEl).height);
     return (w == 0) || (h == 0);
   }
 
   function isOverflowAutoOrScroll(nodeEl) {
     return window.chrome_comp.getComputedStyle(nodeEl).overflow == 'auto' ||
-        window.chrome_comp.getComputedStyle(nodeEl).overflow == 'scroll';
+      window.chrome_comp.getComputedStyle(nodeEl).overflow == 'scroll';
   }
 
   if (Node.ELEMENT_NODE != node.nodeType)
@@ -42,12 +49,11 @@ function checkNode(node, additionalData) {
   if (!isOverflowAutoOrScroll(cb))
     return;
   
-  var nodeBound = node.getBoundingClientRect(),
-      cbBound = cb.getBoundingClientRect();
+  var nodeBound = node.getBoundingClientRect();
+  var cbBound = cb.getBoundingClientRect();
   if (nodeBound.top < cbBound.top || nodeBound.right > cbBound.right ||
       nodeBound.bottom > cbBound.bottom || nodeBound.left < cbBound.left) {
     this.addProblem('BX8037', [node]);
-    
   }
 }
 ); // declareDetector
