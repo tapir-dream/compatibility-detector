efe23317abd8f3d2da851610284aba1a37e2e10a
wangjun02@beyondsoft.com <wangjun02@beyondsoft.com@5629863a-3974-ef24-9cdc-758d184f6d3f>
2010-12-13 03:09:16 +0000

    improve code style.

--- src/detectors/inline_no_relpace_width_height.js
+++ src/detectors/inline_no_relpace_width_height.js
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2010 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 addScriptToInject(function() {
 
 chrome_comp.CompDetect.declareDetector(
@@ -9,32 +25,21 @@ chrome_comp.CompDetect.ScanDomBaseDetector,
 null, // constructor
 
 function checkNode(node, additionalData) {
-  //检查页面是否是混杂模式，如果是标准模式，则返回
-  if(document.compatMode=="CSS1Compat"){
+  //check document type
+  if (document.compatMode == 'CSS1Compat')
     return;
-  }
-  //检查元素是否是 行内非替换元素
-  if (Node.ELEMENT_NODE != node.nodeType || !chrome_comp.isInlineNoReplacedElement(node))
+
+  if (Node.ELEMENT_NODE != node.nodeType || 
+      !chrome_comp.isInlineNoReplacedElement(node))
     return;
-  //行内非替换元素 宽度和高度
+
   var width = chrome_comp.getDefinedStylePropertyByName(node, true, 'width');
   var height = chrome_comp.getDefinedStylePropertyByName(node, true, 'height');
-  //检查当前 行内非替换元素 宽度和高度是否存在，若存在
+
   if((width && width != 'auto') || (height && height != 'auto')){
-    //检查计算后样式 是否是block，若是，则说明position是absolute或fixed，float是left或right，代替下面后两种情况
     var display=chrome_comp.getComputedStyle(node).display;
-    //检查display是否是block，检查position是否是absolute或fixed，float是否left或right
-	/*
-    var display = chrome_comp.getDefinedStylePropertyByName(node, true, 'display');
-    var position = chrome_comp.getDefinedStylePropertyByName(node, true, 'position');
-    var floatCss = chrome_comp.getDefinedStylePropertyByName(node, true, 'float');
-	*/
-    //debugger; 浮动 定位 是否可以由计算后样式display:block 代替
-    //if(display!="block" && position!="absolute" && position!="fixed" && floatCss!="left" && floatCss!="right" )
-    //debugger;
-	if(display == "inline"){
+    if(display == 'inline')
       this.addProblem('RD1014', [node]);
-    }
   }
 }
 ); // declareDetector
