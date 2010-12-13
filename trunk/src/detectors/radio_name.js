b76f9acea215417544cb9539e3d21bf10903af79
duanlixin@beyondsoft.com <duanlixin@beyondsoft.com@5629863a-3974-ef24-9cdc-758d184f6d3f>
2010-12-13 06:41:45 +0000

    修改detector与文件名一致

--- trunk/src/detectors/radio_name.js
+++ trunk/src/detectors/radio_name.js
@@ -4,19 +4,12 @@ addScriptToInject(function() {
 
 chrome_comp.CompDetect.declareDetector(
 
-'inputTypeIsRadioDetector',
+'radio_name',
 
 chrome_comp.CompDetect.ScanDomBaseDetector,
 
 null, // constructor
 
-/*【思路】
- * 检测所有INPUT[type=radio]标记，如果被其 name 值不符合 CDATA 正则表达式则命中
- *
- *【messages.json】
- * "HF9009": { "message": "各浏览器对表单元素单选按钮组设置非 CDATA 标准的 name 属性值解析不同" },
- * "HF9009_suggestion": { "message": "单选按钮的 \"name\" 属性值必须以字母 ([A-Za-z])和数字（[0-9]）开头 ，其后由任何字母、数字、连字符(\"-\")、下划线(\"_\")、冒号(\":\")和句号 (\".\") 组成。" },
- */
 
 function checkNode(node, context) {
 
