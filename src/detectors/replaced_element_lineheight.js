a6d2400b79812ea7b9a573c65419afb182ebd193
duanlixin@beyondsoft.com <duanlixin@beyondsoft.com@5629863a-3974-ef24-9cdc-758d184f6d3f>
2010-12-13 06:43:56 +0000

    修改detector与文件名一致

--- src/detectors/replaced_element_lineheight.js
+++ src/detectors/replaced_element_lineheight.js
@@ -4,23 +4,11 @@ addScriptToInject(function() {
 
 chrome_comp.CompDetect.declareDetector(
 
-'replacedElementLineHeight',
+'replaced_element_lineheight',
 
 chrome_comp.CompDetect.ScanDomBaseDetector,
 
 null, // constructor
-
-/*【思路】
- * 检测所有 type 属性值为 text、password、button、reset、submit 的 INPUT 元素
- * 文本框通过将其 line-height 特性设定为 normal 再恢复判断其是否设定了不为 normal 值的 line-height
- * 按钮通过将其 -webkit-appearance 私有特性设定为 textfield 再回复来判断其是否设定了不为 normal 值的 line-height
- * 若上述表单控件设定了不为 normal 的 line-height 则发出警告
- *
- *【缺陷】
- * 仅考虑了 IE 和 Chrome 之间的差异
- * 没有考虑用户私自设定了 -webkit-appearance 私有特性的情况
- */
-
 
 function checkNode(node, additionalData) {
   function testLineHeightChanged(nodeEl, refValue) {
