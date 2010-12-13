e0a93ff363324dc981d0a900c5ea7804eb9cf4c7
duanlixin@beyondsoft.com <duanlixin@beyondsoft.com@5629863a-3974-ef24-9cdc-758d184f6d3f>
2010-12-13 01:18:42 +0000

    根据修改后的文章，重写该detector

--- src/detectors/word_wrap_spaces.js
+++ src/detectors/word_wrap_spaces.js
@@ -25,73 +25,50 @@ chrome_comp.CompDetect.ScanDomBaseDetector,
 null, // constructor
 
 function checkNode(node, context) {
-  if (context.isDisplayNone() || !node.parentNode || 
-      Node.ELEMENT_NODE != node.parentNode.nodeType)
+  if (Node.ELEMENT_NODE != node.nodeType)
     return;
-  var parentStyle = chrome_comp.getComputedStyle(node.parentNode);
-  if (// About pre white-space there are more problems that are out of range
-      // of this detector.
-      (parentStyle.whiteSpace != 'normal' &&
-       parentStyle.whiteSpace != 'nowrap') ||
-      parentStyle.wordWrap != 'break-word')
-    return;
-
-  // Status values in the block stack:
-  // 1: has met visible element in current logical line
-  // 2: has met trailing space in status 1.
-
-  switch (node.nodeType) {
-    case Node.ELEMENT_NODE:
-      var style = chrome_comp.getComputedStyle(node);
-      switch (style.display) {
-        case 'none':
-          if ((node.tagName == 'INPUT' &&
-               chrome_comp.getAttributeLowerCase(node, 'type') == 'hidden') ||
-              chrome_comp.getDefinedStylePropertyByName(
-                  node, true, 'display') == 'none') {
-            if (context.getValueInBlockStack('status') == 2) {
-              context.putValueInBlockStack('hiddenElementCandidate', node);
-              context.putValueInBlockStack('status', 1);
-            }
-          }
-          // Nodes like SCRIPT, META, etc. should be ignored.
-          break;
-        case 'inline-block':
-          context.clearValuesInBlockStack();
-          context.putValueInBlockStack('status', 1);
-          break;
-        case 'block':
-          // This is a block element in normal flow. Reset the line status
-          // because this block will create a new line.
-          // Not testing fixed position because some browser doesn't support.
-          if (style.position != 'absolute' && style.float == 'none')
-            context.clearValuesInBlockStack();
-          break;
-        case 'inline':
-          if (node.tagName == 'BR') {
-            context.clearValuesInBlockStack();
-          } else if (chrome_comp.isReplacedElement(node)) {
-            context.clearValuesInBlockStack();
-            context.putValueInBlockStack('status', 1);
-          }
-          break;
-      }
-      break;
-    case Node.TEXT_NODE:
-      var status = context.getValueInBlockStack('status');
-      var text = node.textContent;
-      var hiddenElementCandidate =
-        context.getValueInBlockStack('hiddenElementCandidate');
-      if (hiddenElementCandidate && text.match(chrome_comp.LEADING_WHITESPACES))
-        this.addProblem('RT1008', [node]);
-      context.clearValuesInBlockStack();
-      if (!status && text.match(chrome_comp.LEADING_WHITESPACES))
-        status = 1;
-      if (status) {
-        status = text.match(chrome_comp.TRAILING_WHITESPACES) ? 2 : 1;
-        context.putValueInBlockStack('status', status);
-      }
+  function getDefaultDisplay(element){
+    var elementFloat = element.style.float;
+    var elementPosition = element.style.position;
+    var elementDisplay = element.style.display;
+    element.style.float = "none !important";
+    element.style.position = "static !important";
+    element.style.display="";
+    var defaultDisplay = chrome_comp.getComputedStyle(element).display;
+    element.style.float = null;
+    element.style.position = null;
+    element.style.display = null;
+    element.style.float = (elementFloat)?elementFloat:null;
+    element.style.position = (elementPosition)?elementPosition:null;
+    element.style.display = (elementDisplay)?elementDisplay:null;
+    return defaultDisplay;
   }
+  var style = chrome_comp.getComputedStyle(node);
+  var nodeDisplay = style.display;
+  //debugger;
+  var nextSibling = node.nextSibling;
+  var previousSibling = node.previousSibling;
+  var previousreg = /^[\u0020\u0009]/g;
+  var nextreg = /[\u0020\u0009]$/g;
+  if(nodeDisplay == "none" && getDefaultDisplay(node) == "block"){
+    if(nextSibling.nodeType == 3 && previousSibling.nodeType !=3){
+       if(previousreg.test(nextSibling.nodeValue)){
+        this.addProblem('RT1008', [node]);
+      }
+     }
+  }
+
+  if((node.tagName == "INPUT" && node.type == "hidden") ||
+    (getDefaultDisplay(node) == "inline" && nodeDisplay == "none")){
+    if(nextSibling.nodeType == 3 && previousSibling.nodeType == 3){
+       if(previousreg.test(nextSibling.nodeValue)
+         && nextreg.test(previousSibling.nodeValue)){
+        this.addProblem('RT1008', [node]);
+      }
+    }
+  }
+
+
 }
 ); // declareDetector
 
