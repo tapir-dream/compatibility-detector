abad8168429e952f74d9c3c1ef6603bad7383786
wangjun02@beyondsoft.com <wangjun02@beyondsoft.com@5629863a-3974-ef24-9cdc-758d184f6d3f>
2010-12-13 06:55:08 +0000

    improve code style.

--- src/detectors/negative_margin.js
+++ src/detectors/negative_margin.js
@@ -1,28 +1,35 @@
-// @author : luyuan.china@gmail.com
+/*
+ * Copyright 2010 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 addScriptToInject(function() {
 
 chrome_comp.CompDetect.declareDetector(
 
-'negativeMargin',
+'negative_margin',
 
 chrome_comp.CompDetect.ScanDomBaseDetector,
 
 null, // constructor
 
-/*【思路】
- * 检测所有设定了负的 margin 的元素，若其自身在 IE 中触发了 hasLayout 与设定了 position:relative，同时其包含块也触发了 hasLayout 则停止检测
- * 获取元素及其包含块四个方向的绝对位置，若出现位置溢出包含块则发出警告
- *
- */
-
-
 function checkNode(node, additionalData) {
   function isNegativeMargin(nodeEl) {
-    var t = parseInt(window.chrome_comp.getComputedStyle(nodeEl).marginTop),
-        r = parseInt(window.chrome_comp.getComputedStyle(nodeEl).marginRight),
-        b = parseInt(window.chrome_comp.getComputedStyle(nodeEl).marginbottom),
-        l = parseInt(window.chrome_comp.getComputedStyle(nodeEl).marginLeft);
+    var t = parseInt(window.chrome_comp.getComputedStyle(nodeEl).marginTop);
+    var r = parseInt(window.chrome_comp.getComputedStyle(nodeEl).marginRight);
+    var b = parseInt(window.chrome_comp.getComputedStyle(nodeEl).marginbottom);
+    var l = parseInt(window.chrome_comp.getComputedStyle(nodeEl).marginLeft);
     return (t < 0) || (r < 0) || (b < 0) || (l < 0);
   }
 
@@ -32,21 +39,20 @@ function checkNode(node, additionalData) {
 
   if (Node.ELEMENT_NODE != node.nodeType)
     return;
-  
+
   if (!isNegativeMargin(node))
     return;
-  
+
   var cb = window.chrome_comp.getContainingBlock(node);
   if (window.chrome_comp.hasLayoutInIE(cb) && isRelativePositioned(node) &&
       window.chrome_comp.hasLayoutInIE(node))
     return;
 
-  var nodeBound = node.getBoundingClientRect(),
-      cbBound = cb.getBoundingClientRect();
+  var nodeBound = node.getBoundingClientRect();
+  var cbBound = cb.getBoundingClientRect();
   if (nodeBound.top < cbBound.top || nodeBound.right > cbBound.right ||
       nodeBound.bottom > cbBound.bottom || nodeBound.left < cbBound.left) {
     this.addProblem('RB1001', [cb]);
-    
   }
 }
 ); // declareDetector
