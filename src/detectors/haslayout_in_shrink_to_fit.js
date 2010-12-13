a17b7c940eb015a2d5c14b3dc0e9221eb5fb3b33
wangjun02@beyondsoft.com <wangjun02@beyondsoft.com@5629863a-3974-ef24-9cdc-758d184f6d3f>
2010-12-13 09:25:00 +0000

    improve code style.

--- src/detectors/haslayout_in_shrink_to_fit.js
+++ src/detectors/haslayout_in_shrink_to_fit.js
@@ -1,10 +1,24 @@
-// @author : luyuan.china@gmail.com
+/*
+ * Copyright 2010 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 addScriptToInject(function() {
 
 chrome_comp.CompDetect.declareDetector(
 
-'hasLayout_in_shrink_to_fit',
+'haslayout_in_shrink_to_fit',
 
 chrome_comp.CompDetect.ScanDomBaseDetector,
 
@@ -18,7 +32,6 @@ function checkNode(node, additionalData) {
     element.style.display = null;
     element.style.display = (inlineDisplay) ? inlineDisplay : null;
     return width == 'auto';
-    return oldW != (newW + blw + brw + pl + pr);
   }
 
   function isShrinkToFit(element) {
