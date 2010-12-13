af3ae994f2e3a43f5b51f590aa810703d54e3cad
duanlixin@beyondsoft.com <duanlixin@beyondsoft.com@5629863a-3974-ef24-9cdc-758d184f6d3f>
2010-12-13 07:01:51 +0000

    版权，detector名字,中文

--- trunk/src/detectors/static_position.js
+++ trunk/src/detectors/static_position.js
@@ -1,23 +1,28 @@
-// @author : luyuan.china@gmail.com
+/*
+ * Copyright 2010 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 addScriptToInject(function() {
 
 chrome_comp.CompDetect.declareDetector(
 
-'staticPosition',
+'static_position',
 
 chrome_comp.CompDetect.ScanDomBaseDetector,
 
 null, // constructor
-
-/*【思路】
- * 检测所有遵循静态位置定位的绝对定位元素
- * 得到其自身静态时的 display 特性值，以及其上一个非空白节点的 display 特性值，若出现如 RCA 中的差异则发出警告
- *
- *【缺陷】
- * 
- */
-
 
 function checkNode(node, additionalData) {
   function isStaticPosition(element) {
