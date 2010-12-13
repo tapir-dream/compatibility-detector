309fb56655a6b80136f4bd29eef43e6730a838b2
wangjun02@beyondsoft.com <wangjun02@beyondsoft.com@5629863a-3974-ef24-9cdc-758d184f6d3f>
2010-12-13 07:00:16 +0000

    improve code style.

--- src/detectors/nowrap_attribute.js
+++ src/detectors/nowrap_attribute.js
@@ -15,10 +15,13 @@
  */
 
 /*
- * 1. in IE6 IE7 IE8(Q) 'white-space' priority level : CSS property > nowrap attribute
- * 2. object.noWrap = true can't overlap effect of 'white-space' property that set by author,
- *    but it can overlap effect of 'white-space' property through setting by UA(normal) or nowrap attribute(nowrap).
- * 3. set object.noWrap to true or false only affect the 'white-space' property of DIV DD DT.
+ * 1. in IE6 IE7 IE8(Q) 'white-space' priority level: 
+ *    CSS property > nowrap attribute.
+ * 2. object.noWrap = true can't overlap effect of 'white-space' property 
+ *    that set by author, but it can overlap effect of 'white-space' property 
+ *    through setting by UA(normal) or nowrap attribute(nowrap).
+ * 3. set object.noWrap to true or false only affect the 'white-space' 
+ *    property of DIV DD DT.
  */
 addScriptToInject(function() {
 
@@ -32,30 +35,34 @@ null, // constructor
 
 /*
  * report problem if meeting one of the following 2 conditions:
- * 1. If element has nowrap attribute and computed value of 'white-space' property is not 'nowrap'
- *		1.1. Current element DOM object has not noWrap DOM attribute;
- *		1.2. element DOM object has noWrap DOM attribute and attribute value is excluding 
- *           false, 0, '', null, undefined, NaN etc.
- * 2. Author set elementDomObject.noWrap = true and not set 'white-space' property.
- *    This equals to only set nowrap attribute.
+ * 1. If element has nowrap attribute and computed value of 'white-space' 
+ *    property is not 'nowrap'.
+ *    1.1. Current element DOM object has not noWrap DOM attribute;
+ *    1.2. element DOM object has noWrap DOM attribute and attribute value is 
+ *         excluding false, 0, '', null, undefined, NaN etc.
+ * 2. Author set elementDomObject.noWrap = true and not set 'white-space' 
+ *    property. This equals to only set nowrap attribute.
  */
 
 function checkNode(node, context) {
   if(Node.ELEMENT_NODE != node.nodeType)
     return;
   var tagName = node.tagName;
-  //if BODY has nowrap attribute, DIV and P can inherit from BODY and apply 'white-space : nowrap' in IE6 IE7 IE8(Q)
-  //The page may has problem when only detecting BODY 
-  if ((tagName != 'BODY' && tagName != 'DIV' && tagName != 'DT' && tagName != 'DD') || 
-       context.isDisplayNone())
+  // if BODY has nowrap attribute, DIV and P can inherit from BODY and apply 
+  // 'white-space : nowrap' in IE6 IE7 IE8(Q).
+  // The page may has problem when only detecting BODY 
+  if ((tagName != 'BODY' && tagName != 'DIV' && tagName != 'DT' && 
+      tagName != 'DD') || context.isDisplayNone())
     return;
-	
+
   var computedStyle = chrome_comp.getComputedStyle(node);
-  var defStyle = chrome_comp.getDefinedStylePropertyByName(node, '', 'white-space');
-	
-	//main
+  var defStyle = 
+    chrome_comp.getDefinedStylePropertyByName(node, '', 'white-space');
+
+  //main
   if ((node.hasAttribute('nowrap') && computedStyle.whiteSpace != 'nowrap' && 
-      (!node.hasOwnProperty('noWrap') || node.noWrap)) || (node.noWrap && !defStyle) ){
+      (!node.hasOwnProperty('noWrap') || node.noWrap)) || 
+      (node.noWrap && !defStyle)) {
     this.addProblem('HE1003', [node]);
   }
 }
