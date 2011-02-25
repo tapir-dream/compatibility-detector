101dd5517787f4f5d4a04266c5dd5dcc2096edf4
qianbaokun@beyondsoft.com <qianbaokun@beyondsoft.com@5629863a-3974-ef24-9cdc-758d184f6d3f>
2011-02-25 08:19:44 +0000

    modify code to code review recommended contents modfiy english messages.json add i18n key Review URL: http://codereview.appspot.com/4196041

--- trunk/src/detectors/execScript.js
+++ trunk/src/detectors/execScript.js
@@ -14,54 +14,43 @@
  * limitations under the License.
  */
 
+/**
+ * @fileoverview: Check whether the second argument of window.execScript is
+ * valid.
+ * @bug: https://code.google.com/p/compatibility-detector/issues/detail?id=102
+ *
+ * window.execScript can execute non-javascript in IE, but not in Chrome.
+ * Other browsers do not support it. We just check whether the second argument
+ * contains 'javascript' string.
+ */
+
 addScriptToInject(function() {
 
 chrome_comp.CompDetect.declareDetector(
 
-'execScript',
+'execscript',
 
-chrome_comp.CompDetect.ScanDomBaseDetector,
+chrome_comp.CompDetect.NonScanDomBaseDetector,
 
-null, // constructor
+function constructor(){
+  var This = this;
+  this.execScriptHandler_ = function(result, originalArguments, callStack) {
+    if (originalArguments.length < 2)
+      return;
+    var arg1 = originalArguments[1] + '';
+    if (arg1.toLowerCase().indexOf('javascript') == -1)
+      This.addProblem('BX9055');
+  };
+},
 
-function checkNode(node, context) {
-  if (Node.ELEMENT_NODE != node.nodeType)
-    return;
+function setUp() {
+  chrome_comp.CompDetect.registerExistingMethodHook(
+      window, 'execScript', this.execScriptHandler_);
+},
 
-  this.execScriptSyntaxRegexp_ =
-    /([^\w$]*execScript\s?[\(\w$])|([^\w$]*window([.]execScript|\[["']execScript["']\])\s?[\(\w$])/g
-  this.multiLineScriptCommentsRegexp_ = /\/\*([\S\s]*?)\*\//g;
-  this.oneLineScriptCommentsRegexp_ = /[^:\/]\/\/[^\n\r]*/gm;
-
-  var scriptData = '';
-  if (node.tagName == 'SCRIPT') {
-    if (node.src && node.src != '')
-      scriptData = (node.src in context) ? context[node.src] : '';
-    else
-      scriptData = node.text;
-
-    //delete script comment
-    scriptData = scriptData.replace(this.oneLineScriptCommentsRegexp_, '')
-        .replace(this.multiLineScriptCommentsRegexp_, '');
-
-    if (this.execScriptSyntaxRegexp_.test(scriptData)) {
-      this.addProblem('BX9055', [node]);
-      this.execScriptSyntaxRegexp_.test('');
-    }
-  } else {
-    for (var i = 0, l = node.attributes.length; i < l; i++) {
-      if (node.attributes[i].name.toLowerCase().indexOf('on') == 0) {
-        //delete script comment
-        scriptData = node.attributes[i].value
-            .replace(this.oneLineScriptCommentsRegexp_, '')
-            .replace(this.multiLineScriptCommentsRegexp_, '');
-      }
-      if (this.execScriptSyntaxRegexp_.test(scriptData)) {
-        this.addProblem('BX9055', [node]);
-        this.execScriptSyntaxRegexp_.test('');
-      }
-    }
-  }
+function cleanUp() {
+  chrome_comp.CompDetect.unregisterExistingMethodHook(
+      window, 'execScript', this.execScriptHandler_);
 }
 ); // declareDetector
 
