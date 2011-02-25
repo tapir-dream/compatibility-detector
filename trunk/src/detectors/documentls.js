ca9fc5dfd23b4665290c2a5211203ca8df63b019
luyuan01@beyondsoft.com <luyuan01@beyondsoft.com@5629863a-3974-ef24-9cdc-758d184f6d3f>
2011-02-25 06:25:06 +0000

    SD9008 document_ls detector One detector implementation for checking the load method of the Document object created by the createDocument method of the DOMImplementation object.
    Review URL: http://codereview.appspot.com/4202041

--- trunk/src/detectors/documentls.js
+++ trunk/src/detectors/documentls.js
@@ -14,9 +14,28 @@
  * limitations under the License.
  */
 
+/**
+ * @fileoverview: Check the load method of the Document object created by the
+ * createDocument method of the DOMImplementation object.
+ * @bug: https://code.google.com/p/compatibility-detector/issues/detail?id=123
+ * 
+ * Chrome supports to use document.implementation.createDocument to create a
+ * DOMImplementation object associated with the current document, its returned
+ * value is a Document object, but this object does not have the load method
+ * which is described in the W3C deprecated Working Draft (refer to
+ * http://www.w3.org/TR/2003/WD-DOM-Level-3-LS-20030619/load-save.html
+ * #LS-DocumentLS).
+ * First Hook the existing createDocument method of the
+ * DOMImplementation.prototype object, second, hook the load property of the
+ * Document.prototype in its handler, and report the issue in the second
+ * handler.
+ */
+
+
 addScriptToInject(function() {
 
-var hooked = false;
+// The flag for the load method hook.
+var documentHooked = false;
 
 chrome_comp.CompDetect.declareDetector(
 
@@ -27,27 +46,36 @@ chrome_comp.CompDetect.NonScanDomBaseDetector,
 function constructor(rootNode) {
   var This = this;
   this.createDocumentHandler_ = function(result, originalArguments, callStack) {
-    if (!hooked)
+    // To avoid hooking repeatedly.
+    if (!documentHooked) {
+      // Use registerSimplePropertyHook because the load method of the Document
+      // object is not a exist method, so we use the property hook which can
+      // hook its execution too.
       chrome_comp.CompDetect.registerSimplePropertyHook(
           Document.prototype, 'load', This.loadHandler_);
+    }
   };
   this.loadHandler_ = function(oldValue, newValue, reason) {
-    hooked = true;
+    documentHooked = true;
     This.addProblem('SD9008', { nodes: [this], needsStack: true });
   };
 },
 
 function setUp() {
   chrome_comp.CompDetect.registerExistingMethodHook(
-      DOMImplementation.prototype, 'createDocument', this.createDocumentHandler_);
+      DOMImplementation.prototype, 'createDocument',
+      this.createDocumentHandler_);
 },
 
 function cleanUp() {
   chrome_comp.CompDetect.unregisterExistingMethodHook(
-      DOMImplementation.prototype, 'createDocument', this.createDocumentHandler_);
-  if (hooked)
+      DOMImplementation.prototype, 'createDocument',
+      this.createDocumentHandler_);
+  if (documentHooked) {
     chrome_comp.CompDetect.unregisterSimplePropertyHook(
         Document.prototype, 'load', this.loadHandler_);
+    documentHooked = false;
+  }
 }
 ); // declareDetector
 
