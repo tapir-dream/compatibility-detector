db4182706b5891c674175b658a3bf6f07fb4a256
luyuan01@beyondsoft.com <luyuan01@beyondsoft.com@5629863a-3974-ef24-9cdc-758d184f6d3f>
2011-03-11 09:51:27 +0000

    [RD8001] [HG8001] [RX1010] detect "document type and boxsizing" problems Detect 'RD8001 HG8001 RX1010: document type and boxsizing'. BUG=https://code.google.com/p/compatibility-detector/issues/detail?id=56 BUG=https://code.google.com/p/compatibility-detector/issues/detail?id=57 BUG=https://code.google.com/p/compatibility-detector/issues/detail?id=149 TEST=None
    Review URL: http://codereview.appspot.com/4240060

--- trunk/src/detectors/document_type_and_boxsizing.js
+++ trunk/src/detectors/document_type_and_boxsizing.js
@@ -15,11 +15,11 @@
  */
 
 /**
- * @fileoverview: One detector implementation for checking the real document
- * mode in IE and WebKit browsers, and the box which the 'width' and 'height'
- * properties apply.
- * @bug: https://code.google.com/p/compatibility-detector/issues/detail?id=56
- * @bug: https://code.google.com/p/compatibility-detector/issues/detail?id=57
+ * @fileoverview Check the real document mode in IE and WebKit browsers, and
+ * the box which the 'width' and 'height' properties apply.
+ * @bug https://code.google.com/p/compatibility-detector/issues/detail?id=56
+ * @bug https://code.google.com/p/compatibility-detector/issues/detail?id=57
+ * @bug https://code.google.com/p/compatibility-detector/issues/detail?id=149
  *
  * The comment or XML declaration before DTD will make the DTD be invalid in IE
  * so that the HTML document will be in quirks mode in IE. We must get the real
@@ -35,28 +35,6 @@
  */
 
 addScriptToInject(function() {
-
-/**
- * The comment or XML declaration before DTD will make the DTD be invalid in IE
- * so that the HTML document will be in quirks mode in IE.
- */
-function hasCommentBeforeDTD(element) {
-  var prev = element;
-  if (!prev)
-    return;
-  while (prev.previousSibling)
-    prev = prev.previousSibling;
-  if (prev && prev.nodeType == 8 && !chrome_comp.inQuirksMode()) {
-    var comm = prev.nodeValue.split(/\s+/);
-    if (comm.length < 1)
-      return prev;
-    if (comm[0] != '[if')
-      return prev;
-    if (/^!IE/g.test(comm[1]))
-      return;
-    return prev;
-  }
-}
 
 function hasHorizontalBorder(element) {
   var style = chrome_comp.getComputedStyle(element);
@@ -86,52 +64,106 @@ function hasVerticalPadding(element) {
   return paddingTop || paddingBottom;
 }
 
-function getRealComputedWidthAndHeight(element) {
-  var x = element.cloneNode(false);
-  x.style.display = 'none !important';
-  var parent = element.parentElement;
-  if (!parent)
+/**
+ * Check If the image has padding because the images cannot be set padding when
+ * IE is Quirks Mode. Refer to:
+ * http://msdn.microsoft.com/en-us/library/bb250395(v=vs.85).aspx
+ * #cssenhancements_topic3.
+ * @param {object} element the DOM element to test
+ * @return {object} an object storing if the element is an image element and has
+ *     padding.
+ */
+function checkPaddingOnImage(element) {
+  var result = {isImage: false, hasPadding: false};
+  if (element.tagName != 'IMG')
+    return result;
+  result.isImage = true;
+  result.hasPadding = hasHorizontalPadding(element) ||
+      hasVerticalPadding(element);
+  return result;
+}
+
+/**
+ * Do not consider the non-replaced inline elements, table elements and the
+ * invisible elements, because:
+ * 1. The 'width' and 'height' can not apply to the non-replaced inline
+ *    elements, table rows, and row groups. Refer to:
+ *    http://www.w3.org/TR/CSS21/visudet.html#the-width-property
+ * 2. The visual layout of table contents is different from the visual layout
+ *    of others. It has the particular width and height algorithms. We need to
+ *    detect the table elements separately. Refer to:
+ *    http://www.w3.org/TR/CSS21/tables.html#table-layout
+ * 3. Since a display of 'none' causes an element to not appear, its
+ *    descentant elements do not generate any boxes either, so for these
+ *    elements, we do not need to detect. Refer to:
+ *    http://www.w3.org/TR/CSS21/visuren.html#display-prop
+ */
+function isBlockOrReplacedElement(styleObject, element) {
+  if (!styleObject || typeof styleObject != 'object')
+    return false;
+  var display = styleObject.display;
+  if (!display)
+    return false;
+  if ((display == 'inline' && !chrome_comp.isReplacedElement(element)) ||
+      display == 'none' || chrome_comp.isTableElement(element))
+    return false;
+  return true;
+}
+
+/**
+ * The 'auto' width and height do not have this compatibility issue. Since IE
+ * in Quirks Mode includes the content, padding and borders within a specified
+ * width or height, if an element does not have the horizontal padding and
+ * borders, in other words, the horizontal content box and border box are
+ * overlaps, and the same on vertical.
+ */
+function hasPaddingOrBorderWithNonAutoWidthAndHeight(styleObject, element) {
+  if (!styleObject || typeof styleObject != 'object')
+    return false;
+  var width = styleObject.width;
+  var height = styleObject.height;
+  if (!width || !height)
+    return false;
+  var hasHorizontalProblem = width != 'auto' &&
+      (hasHorizontalPadding(element) || hasHorizontalBorder(element));
+  var hasVerticalProblem = height != 'auto' &&
+      (hasVerticalPadding(element) || hasVerticalBorder(element));
+  if (hasHorizontalProblem || hasVerticalProblem)
     return true;
-  if (parent.lastElementChild === element)
-    parent.appendChild(x);
-  else
-    parent.insertBefore(x, element);
-  var width = chrome_comp.getComputedStyle(x).width;
-  var height = chrome_comp.getComputedStyle(x).height;
-  parent.removeChild(x);
-  x = null;
-  return { width: width, height: height };
+  // TODO: Do consider the container stretched by its children when IE is Quirks
+  // Mode.
+  return false;
 }
 
-function isWidthStretched(element) {
-  var ch = element.children;
-  var w = parseInt(chrome_comp.getComputedStyle(element).width, 10);
-  if (ch.length < 1)
-    return;
-  for (var i = 0, j = ch.length; i < j; i++) {
-    var width = ch[i].offsetWidth;
-    if (width >= w)
-      return true;
-  }
+/**
+ * Check If the iframe has padding because the 'width' and 'height' of the
+ * IFRAME elements apply on padding box when IE is Quirks Mode.
+ * @param {object} element the DOM element to test
+ * @return {object} an object storing if the element is an iframe element and
+ *     has padding.
+ */
+function checkPaddingOnIframe(element) {
+  var result = {isIframe: false, hasPadding: false};
+  if (element.tagName != 'IFRAME')
+    return result;
+  result.isIframe = true;
+  result.hasPadding = hasHorizontalPadding(element) ||
+      hasVerticalPadding(element);
+  return result;
 }
 
-function isHeightStretched(element) {
-  var ch = element.children;
-  var h = parseInt(chrome_comp.getComputedStyle(element).height, 10);
-  var lh = parseInt(chrome_comp.getComputedStyle(element).lineHeight, 10);
-  if (lh >= h)
+/**
+ * Some authors may use CSS3 box-sizing property to make the values of width and
+ * height apply on border box in WebKit browsers. So we must ignore this
+ * situation. We should also consider -webkit-box-sizing property for the older
+ * Chrome.
+ */
+function isBoxSizingOnBorderBox(element) {
+  var boxSizing = chrome_comp.getComputedStyle(element).boxSizing;
+  var webkitBoxSizing = chrome_comp.getComputedStyle(element).webkitBoxSizing;
+  if (boxSizing == 'border-box' || webkitBoxSizing == 'border-box')
     return true;
-  if (ch.length < 1)
-    return;
-  for (var i = 0, j = ch.length; i < j; i++) {
-    var height = ch[i].offsetHeight;
-    if (height >= h)
-      return true;
-  }
-}
-
-function isTableElement(element) {
-  return chrome_comp.getComputedStyle(element).display.indexOf('table') != -1;
+  return false;
 }
 
 chrome_comp.CompDetect.declareDetector(
@@ -141,55 +173,51 @@ chrome_comp.CompDetect.declareDetector(
 chrome_comp.CompDetect.ScanDomBaseDetector,
 
 function constructor(rootNode) {
-  // Must save this state because it is useful in checkNode function.
-  this.commentBeforeDTD = hasCommentBeforeDTD(rootNode);
-  // There are several strange DTDs that have the document mode in IE and
-  // non-IE browser be different. And we must know the real document mode
-  // triggered by the present DTD in IE and WebKit browsers.
-  this.doctypeInIE = '';
-  this.doctypeInWebKit = '';
-  var diffMap;
-  var doctype = document.doctype;
-  var compatMode = document.compatMode.toLowerCase();
-  var publicId = (doctype) ? doctype.publicId : 0;
-  var systemId = (doctype) ? doctype.systemId : 0;
-  this.doctypeInIE = this.doctypeInWebKit =
-      (compatMode == 'backcompat') ? 'Q' : 'S';
-  if (this.commentBeforeDTD)
-    this.doctypeInIE = 'Q';
-  diffMap = {
-    '-//W3C//DTD HTML 4.0 Transitional//EN': {
-      'systemId': 'http://www.w3.org/TR/html4/loose.dtd',
-      'IE': 'S',
-      'WebKit': 'Q'
-    },
-    'ISO/IEC 15445:2000//DTD HTML//EN': {
-      'systemId': '',
-      'IE': 'Q',
-      'WebKit': 'S'
-    },
-    'ISO/IEC 15445:1999//DTD HTML//EN': {
-      'systemId': '',
-      'IE': 'Q',
-      'WebKit': 'S'
-    },
-    '-//W3C//DTD HTML 4.0 Transitional//EN': {
-      'systemId': 'http://www.w3.org/TR/REC-html40/loose.dtd',
-      'IE': 'S',
-      'WebKit': 'Q'
-    }
-  }
-  if (diffMap[publicId]) {
-    if (diffMap[publicId]['systemId'] == systemId) {
-      this.doctypeInIE = diffMap[publicId]['IE'];
-      this.doctypeInWebKit = diffMap[publicId]['WebKit'];
-    }
-  }
-  if (doctype && doctype.name ==
-      '"xmlns:xsl=\'http://www.w3.org/1999/xsl/transform\'"') {
-    this.doctypeInIE = 'S';
-    this.doctypeInWebKit = 'Q';
-  }
+  // Save some important values for detecting each node here.
+  this.doctypeInIE = chrome_comp.documentMode.IE;
+  this.doctypeInWebKit = chrome_comp.documentMode.WebKit;
+
+  // Detect HG8001 issue. More information about the DTD, please refer to the
+  // documentMode part in framework_shared.js.
+  if (chrome_comp.documentMode.hasCommentBeforeDTD)
+    this.addProblem('HG8001');
+
+  // There is no difference in Standards Mode. We can not detect RD8001 issue
+  // If the document mode is undefined in IE. More information about RD8001,
+  // please refer to:
+  // http://www.w3help.org/zh-cn/causes/RD8001
+  // About IE box model bug:
+  //  IE Quirks Mode: border-box                Others: content-box
+  //
+  //                                     ---------------------------------
+  //                                     |            margin             |
+  //                                     | ----------------------------- |
+  //                                     | |          border           | |
+  //   -------------------------         | | ------------------------- | |
+  //   |        margin         |         | | |        padding        | | |
+  //   | --------------------- |  -----  | | | --------------------- | | |
+  //   | |      border       | |    |    | | | |      content      | | | |
+  //   | | ----------------- | |    |    | | | |                   | | | |
+  //   | | |    padding    | | |    2    | | | |                   | | | |
+  //   | | | ------------- | | |    0    | | | |                   | | | |
+  //   | | | |  content  | | | |    0    | | | |                   | | | |
+  //   | | | |           | | | |    p    | | | |                   | | | |
+  //   | | | |           | | | |    x    | | | |                   | | | |
+  //   | | | ------------- | | |    |    | | | |                   | | | |
+  //   | | ----------------- | |    |    | | | |<======150px======>| | | |
+  //   | --------------------- |  -----  | | | --------------------- | | |
+  //   | |<======150px======>| |         | | ------------------------- | |
+  //   -------------------------         | ----------------------------- |
+  //                                     ---------------------------------
+  // IE box model bug is a software bug in the implementation of CSS in earlier
+  // versions of IE. IE6 and newer are not affected in their Standards Mode,
+  // but for compatibility reasons, the bug is still present when a page is
+  // rendered in Quirks Mode. If doctypeInIE is undefined, in this case, we are
+  // not sure that IE is rendering in quirks mode. So ignore it.
+  // More information refer to:
+  // http://en.wikipedia.org/wiki/Internet_Explorer_box_model_bug
+  this.isIEInQuirksMode = this.doctypeInIE && !(this.doctypeInIE == 'S' &&
+      this.doctypeInWebKit == 'S');
 },
 
 function checkNode(node, context) {
@@ -198,83 +226,70 @@ function checkNode(node, context) {
 
   var tag = node.tagName;
 
-  if (tag == 'SCRIPT')
+  // Only check when IE is Quirks Mode.
+  if (!this.isIEInQuirksMode)
     return;
 
-  var inputType = '';
-  if (tag == 'HTML') {
-    if (this.commentBeforeDTD) {
-      this.addProblem('HG8001', [node]);
+  var specifiedValue = chrome_comp.getSpecifiedValue(node);
+
+  var imagePadding = checkPaddingOnImage(node);
+  if (imagePadding.isImage) {
+    if (imagePadding.hasPadding) {
+      this.addProblem('RX1010', {
+        nodes: [node],
+        details: '{padding: ' + specifiedValue.paddingTop + ' ' +
+            specifiedValue.paddingRight + ' ' + specifiedValue.paddingBottom +
+            ' ' + specifiedValue.paddingLeft + ';}'
+      });
     }
+    // The 'width' and 'height' of images apply on the content box in all
+    // browsers. In following cases, there are no differences.
     return;
   }
 
-  if (this.doctypeInIE == 'S' && this.doctypeInWebKit == 'S')
-    return
-
-  var display = chrome_comp.getComputedStyle(node).display;
-  if (display == 'inline' || display == 'none')
+  if (!isBlockOrReplacedElement(specifiedValue, node))
+    return;
+  if (!hasPaddingOrBorderWithNonAutoWidthAndHeight(specifiedValue, node))
     return;
 
-  if (isTableElement(node))
+  var details = '{width: ' + specifiedValue.width +
+      ';\n border-left-width: ' + specifiedValue.borderLeftWidth +
+      ';\n padding-left: ' + specifiedValue.paddingLeft +
+      ';\n padding-right: ' + specifiedValue.paddingRight +
+      ';\n border-right-width: ' + specifiedValue.borderRightWidth + ';\n\n' +
+      'height: ' + specifiedValue.height +
+      ';\n border-top-width: ' + specifiedValue.borderTopWidth +
+      ';\n padding-top: ' + specifiedValue.paddingTop +
+      ';\n padding-bottom: ' + specifiedValue.paddingBottom +
+      ';\n border-bottom-width: ' + specifiedValue.borderBottomWidth + ';}';
+
+  var iframePadding = checkPaddingOnIframe(node);
+  if (iframePadding.isIframe) {
+    if (iframePadding.hasPadding)
+      this.addProblem('RD8001', {nodes: [node], details: details});
+    return;
+  }
+
+  if (isBoxSizingOnBorderBox(node))
     return;
 
-  var real = getRealComputedWidthAndHeight(node);
-  if (real.width == 'auto' && real.height == 'auto')
-    return;
-  if (real.width != 'auto' && !hasHorizontalPadding(node) &&
-      !hasHorizontalBorder(node))
-    return;
-  if (real.height != 'auto' && !hasVerticalPadding(node) &&
-      !hasVerticalBorder(node))
-    return;
-  if (real.width != 'auto' && isWidthStretched(node))
-    return;
-  if (real.height != 'auto' && isHeightStretched(node))
-    return;
-
-  if (tag == 'INPUT')
-    inputType = chrome_comp.getAttributeLowerCase(node, 'type');
-
-  // In following cases, there are no differences in all browsers.
-  if (tag == 'TABLE' || tag == 'IMG')
-    return;
-
-  // Some authors may use -webkit-box-sizing property to make the value of width
-  // and height apply on border box in WebKit browsers. So we must ignore this
-  // situation.
-  var boxSizing = chrome_comp.getComputedStyle(node).webkitBoxSizing;
-
-  var isButton;
-  var isTextBox;
-  if ((tag == 'BUTTON') || (tag == 'INPUT' && (inputType == 'button' ||
-      inputType == 'submit' || inputType == 'reset')))
-    isButton = true;
-  if ((tag == 'TEXTAREA') || (tag == 'INPUT' && (inputType == 'text' ||
-      inputType == 'password')))
-    isTextBox = true;
-
+  // Here the node's 'box-sizing' in WebKit is content box.
   if (!chrome_comp.isReplacedElement(node)) {
-    if (this.doctypeInIE == 'Q' && boxSizing != 'border-box') {
-      this.addProblem('RD8001', [node]);
-      return;
-    }
-    if (this.doctypeInIE == 'S' && boxSizing == 'border-box') {
-      this.addProblem('RD8001', [node]);
-      return;
-    }
+    this.addProblem('RD8001', {nodes: [node], details: details});
   } else {
-    if (isTextBox && (this.doctypeInIE != this.doctypeInWebKit)) {
-      if (this.doctypeInIE == 'Q' && boxSizing != 'border-box')
-        this.addProblem('RD8001', [node]);
-      return;
-    } else if (isButton && (this.doctypeInIE != this.doctypeInWebKit)) {
-      if (this.doctypeInIE == 'Q' && boxSizing != 'border-box')
-        this.addProblem('RD8001', [node]);
-      return;
-    } else if (tag == 'IFRAME' && this.doctypeInIE == 'Q' && hasPadding(node)) {
-      this.addProblem('RD8001', [node]);
-      return;
+    // the OBJECT and EMBED are quite stange, the 'width' and 'height' apply to
+    // border-box subtracting the scrollbar's size. So If there are OBJECT and
+    // EMBED set 'width' or 'height' and borders or padding, we should report
+    // this issue.
+    if (tag == 'OBJECT' || tag == 'EMBED') {
+      this.addProblem('RD8001', {nodes: [node], details: details});
+    } else {
+      // For other replaced elements, we report the issue when Chrome is
+      // Standards Mode and '-webkit-box-sizing:content-box', because the
+      // 'width' and 'height' apply to border-box for these elements in IE's
+      // Quirks Mode.
+      if (this.doctypeInWebKit == 'S')
+        this.addProblem('RD8001', {nodes: [node], details: details});
     }
   }
 }
