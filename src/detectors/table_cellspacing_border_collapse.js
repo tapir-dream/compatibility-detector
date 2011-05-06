4a71619c73e2a6d95960d3c912d3ef30088daf9c
qianbaokun@beyondsoft.com <qianbaokun@beyondsoft.com@5629863a-3974-ef24-9cdc-758d184f6d3f>
2011-05-06 09:28:58 +0000

    [RX1008|RE1020] Detector table_cellspacing_border_collapse BUG=https://code.google.com/p/compatibility-detector/issues/detail?id=24 BUG=https://code.google.com/p/compatibility-detector/issues/detail?id=119 TEST=None
    Review URL: http://codereview.appspot.com/4287083

--- src/detectors/table_cellspacing_border_collapse.js
+++ src/detectors/table_cellspacing_border_collapse.js
@@ -15,17 +15,16 @@
  */
 
 /**
- * @fileoverview: One detector implementation for checking problems about
- * 'border-spacing' property and cellspacing attribute in the collapsing border
- * model.
- * @bug: https://code.google.com/p/compatibility-detector/issues/detail?id=24
- * @bug: https://code.google.com/p/compatibility-detector/issues/detail?id=119
+ * @fileoverview Check problems about 'border-spacing' property and cellspacing
+ * attribute in the collapsing border model.
+ * @bug https://code.google.com/p/compatibility-detector/issues/detail?id=24
+ * @bug https://code.google.com/p/compatibility-detector/issues/detail?id=119
  *
- * 'border-spacing' property specifies the horizontal and vertical distance that
- * separates adjoining cell borders. The effect of setting cellspacing attribute
- * is the same as set 'border-spacing' property. IE6 IE7 IE8(Q) doesn't support
- * 'border-spacing' property, and cellspacing attribute is still works in the
- * collapsing border model('border-collapse:collapse').
+ * 'border-spacing' property specifies the horizontal and vertical distance
+ * that separates adjoining cell borders. The effect of setting cellspacing
+ * attribute is the same as set 'border-spacing' property. IE6 IE7 IE8(Q)
+ * doesn't support 'border-spacing' property, and cellspacing attribute is
+ * still works in the collapsing border model('border-collapse:collapse').
  *
  * First we check all 'display : table' and 'display : inline-table' elements,
  * then detect problems in two cases:
@@ -35,9 +34,9 @@
  * attribute cellspacing and it's value is unequal to the value of
  * border-horizontal-spacing or border-vertical-spacing, then report problem
  * RE1020. If the table-like-element has no attribute cellspacing and the value
- * of 'border-horizontal-spacing' or 'border-vertical-spacing' greater than 2px,
- * then report problem RE1020(TABLE has 2px border-spacing default in IE and
- * omiting difference causes by 1px).
+ * of 'border-horizontal-spacing' or 'border-vertical-spacing' greater than
+ * 2px, then report problem RE1020(TABLE has 2px border-spacing default in IE
+ * and omiting difference causes by 1px).
  */
 
 addScriptToInject(function() {
@@ -48,52 +47,64 @@ chrome_comp.CompDetect.declareDetector(
 
 chrome_comp.CompDetect.ScanDomBaseDetector,
 
-null, // constructor
+function constructor() {
+  this.THRESHOLD_OF_COLLAPSE = 2;
+  this.THRESHOLD_OF_SEPARATE = 5;
+  this.reportProblemRE1020 = function(node, hSpacing, vSpacing) {
+    this.addProblem('RE1020', {
+      nodes: [node],
+      details: 'hSpacing = ' + hSpacing + 'px' +
+         ', vSpacing = ' + vSpacing + 'px'
+    });
+  }
+},
 
 function checkNode(node, context) {
-  if (Node.ELEMENT_NODE != node.nodeType)
-    return;
-
-  if (context.isDisplayNone())
+  if (Node.ELEMENT_NODE != node.nodeType || context.isDisplayNone())
     return;
 
   var computedStyle = chrome_comp.getComputedStyle(node);
   var display = computedStyle.display;
   if (display == 'table' || display == 'inline-table') {
     var borderCollapse = computedStyle.borderCollapse;
+    var cellspacing = node.getAttribute('cellspacing');
     switch (borderCollapse) {
       case 'collapse':
-        if (node.hasAttribute('cellspacing') &&
-            parseInt(node.getAttribute('cellspacing'), 10) > 0 &&
+        if (chrome_comp.toInt(cellspacing) > this.THRESHOLD_OF_COLLAPSE &&
             node.childElementCount > 0) {
-          this.addProblem('RX1008', [node]);
+          this.addProblem('RX1008', {
+            nodes: [node],
+            details: 'cellspacing = ' + cellspacing
+          });
         }
         break;
       case 'separate':
-        var hSpacing =
-            parseInt(computedStyle.WebkitBorderHorizontalSpacing, 10) | 0;
-        var vSpacing =
-            parseInt(computedStyle.WebkitBorderVerticalSpacing, 10) | 0;
+        var hSpacing = chrome_comp.toInt(
+            computedStyle.WebkitBorderHorizontalSpacing, 10);
+        var vSpacing = chrome_comp.toInt(
+            computedStyle.WebkitBorderVerticalSpacing, 10);
 
         if (node.tagName == 'TABLE') {
           if (node.hasAttribute('cellspacing')) {
-            var spacing = parseInt(node.getAttribute('cellspacing', 10)) | 0;
-            if ((hSpacing && hSpacing != spacing) ||
-                (vSpacing && vSpacing != spacing)) {
-              this.addProblem('RE1020', [node]);
+            var spacing = chrome_comp.toInt(node.getAttribute('cellspacing'));
+            if (hSpacing != spacing || vSpacing != spacing) {
+              this.reportProblemRE1020(node, hSpacing, vSpacing);
               return;
             }
           } else {
-            // TABLE has 2px border-spacing default in IE and omiting difference
-            // causes by 1px
-            if (hSpacing > 2 || vSpacing > 2) {
-              this.addProblem('RE1020', [node]);
+            // TABLE has 2px border-spacing default in IE and omiting
+            // difference causes by 1px.
+            if (hSpacing > this.THRESHOLD_OF_SEPARATE ||
+                vSpacing > this.THRESHOLD_OF_SEPARATE) {
+              this.reportProblemRE1020(node, hSpacing, vSpacing);
               return;
             }
           }
-        // Other elements which set 'display:table' or 'display:inline-table'
-        } else if (hSpacing || vSpacing) {
-          this.addProblem('RE1020', [node]);
+        // Other elements which set 'display:table' or 'display:inline-table'.
+        } else {
+          if (hSpacing > this.THRESHOLD_OF_SEPARATE ||
+              vSpacing > this.THRESHOLD_OF_SEPARATE)
+            this.reportProblemRE1020(node, hSpacing, vSpacing);
         }
         break;
     }
